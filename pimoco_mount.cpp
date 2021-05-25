e80a8c481eed7b2c83d83f4b561abfcec7ad769b
Markus Noga <ml@noga.de>
2021-05-25 09:31:34 +0200

    #32: added axis names for tracing

--- pimoco_mount.cpp
+++ pimoco_mount.cpp
@@ -91,7 +91,7 @@ void ISSnoopDevice(XMLEle *root) {
 // Public class members
 //
 
-PimocoMount::PimocoMount() : stepperHA(getDeviceName(), HA_DIAG0_PIN), stepperDec(getDeviceName(), DEC_DIAG0_PIN),
+PimocoMount::PimocoMount() : stepperHA(getDeviceName(), "HA", HA_DIAG0_PIN), stepperDec(getDeviceName(), "Dec", DEC_DIAG0_PIN),
     spiDeviceFilenameHA("/dev/spidev0.0"), spiDeviceFilenameDec("/dev/spidev0.1") {
 	setVersion(CDRIVER_VERSION_MAJOR, CDRIVER_VERSION_MINOR);
 
